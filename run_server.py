a547ed6bbfa3d2f296ae64f9c52eefe3eae48a47
Rosmary <rosmarygp11@gmail.com>
2026-05-06 17:45:57 +0100

    identifica pisos con observaciones viables

--- run_server.py
+++ run_server.py
@@ -1,18 +1,16 @@
 import uvicorn
 import sys
 import os
+from backend.app.main import app
 
-# Add project root to path
-project_root = os.path.dirname(os.path.abspath("."))
+project_root = os.path.dirname(os.path.abspath(__file__))
+
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-# Add backend folder to path for imports
 backend_path = os.path.join(project_root, "backend")
 if backend_path not in sys.path:
     sys.path.insert(0, backend_path)
-
-from backend.app.main import app
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
