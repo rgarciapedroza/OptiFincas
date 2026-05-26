6cba00e2d467213f35e24cbec052fa86fdab7923
Rosmary <rosmarygp11@gmail.com>
2026-05-26 13:47:19 +0100

    API proxy

--- backend/__init__.py
+++
@@ -1,3 +0,0 @@
-# backend/app/schemas/__init__.py
-from .models import PisoCreate, PisoUpdate, MovimientoResponse, MovimientoClasificado, ResumenCategoria, ResumenPiso, ErrorResponse, SuccessResponse
-from .extracto import FinanzasReportRequest, MovimientoClasificado as MovimientoClasificadoExtracto
